be681f4d3795f47899c9773a28b9ba315652c040
Kyle <tigerlove95@naver.com>
2021-09-30 01:38:40 +0900

    210930

--- prepare/front/pages/_error.js
+++ prepare/front/pages/_error.js
@@ -7,7 +7,7 @@ const Page = ({ statusCode }) => {
 };
 
 Page.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : [404, 500];
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
 
