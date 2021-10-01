ccea50492dff9bb31d653c60b41cacc174bfe282
Kyle <tigerlove95@naver.com>
2021-10-01 19:07:58 +0900

    211001

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -29,16 +29,22 @@ if (process.env.NODE_ENV === "production") {
   app.use(morgan("combined"));
   app.use(hpp());
   app.use(helmet());
+  app.use(
+    cors({
+      origin: "http://kanghun.xyz",
+      credentials: true,
+    })
+  );
 } else {
   app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
 }
 
-app.use(
-  cors({
-    origin: ["http://kanghun.xyz", "http://localhost:3000"],
-    credentials: true,
-  })
-);
 app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(morgan("dev"));
 
